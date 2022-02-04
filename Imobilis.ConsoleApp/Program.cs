f7876d6f5208d2f3cc96215c1f73732a3fd4a6e4
Mariaeduardaz <vitorvogel12@gmail.com>
2022-02-04 17:02:43 -0300

    Add project files.

---
+++ Imobilis.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Imobilis.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Contador de área");
+
+            Console.WriteLine("Para descobrir a área do terreno:");
+
+            Console.Write("Digite a Largura do terreno: ");
+
+            float Largura = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Comprimento do terreno: ");
+
+            float Comprimento = float.Parse(Console.ReadLine());
+
+            float resultado = Largura * Comprimento;
+
+            Console.WriteLine(resultado );
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
